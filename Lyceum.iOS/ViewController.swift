a26a5a5905bc6ebc6388136a78f2608b39ac97fa
Wahab Syed <wahab.syed@neudesic.com>
2019-02-22 15:54:29 -0800

    first commit

---
+++ Lyceum.iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lyceum.iOS
+//
+//  Created by Wahab Syed on 9/15/16.
+//  Copyright © 2016 Wahab Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
